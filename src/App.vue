7ccf8422fc150b91d18b85329ece6abf2d385d20
Ana <abarba1@trinity.edu>
2026-03-04 20:23:43 +0100

    Add QuestionCard component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
     <h1>QuizBlitz</h1>
-    <!-- MILESTONE 2 -->
-    <StartScreen v-on:start="handleStart" />
-    <ScoreBoard v-on:restart="resetGame" :score="score"/>
+    <!-- MILESTONE 2 
+    StartScreen should show when showStartScreen is true
+
+    -->
+    <StartScreen v-if="showStartScreen" v-on:start="handleStart"/>
+    <ScoreBoard v-if="!showStartScreen" v-on:restart="resetGame" :score="score"/>
+    <!-- MILESTONE 3 
+    QuestionCard should show when showStartScreen
+    -->
+    <QuestionCard v-if="!showStartScreen" v-on:answer="handleAnswer" />
   </div>
 </template>
 
@@ -11,13 +18,15 @@
 // MILESTONE 2
 import StartScreen from './components/StartScreen.vue'
 import ScoreBoard from './components/ScoreBoard.vue';
+import QuestionCard from './components/QuestionCard.vue'
 
 export default {
   name: 'App',
   //MILESTONE 2
   components: {
     StartScreen,
-    ScoreBoard
+    ScoreBoard,
+    QuestionCard
   },
   //data returns an object contaiing the reactive variables you want to use in your template or methods
   data() { 
@@ -28,11 +37,24 @@ export default {
   },
   methods: {
     handleStart() {
-      console.log('Play button clicked')
+      console.log("Play button clicked")
+      this.showStartScreen = false
     },
-
+    //runs when play is clocked, so we want to hide the start screen
+    handleAnswer(isCorrect) {
+      console.log(this.score)
+        if (isCorrect) {
+          console.log("correct: +1")
+          this.score++
+          console.log(this.score)
+        } else {
+          console.log("wrong: +0")
+          console.log(this.score)
+        }
+    },
+    
     resetGame(){
-      console.log("Game is restarting!")
+      console.log("Game is restarting")
       this.score = 0
       this.showStartScreen = true
     }
---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,70 @@
+<!-- MILESTONE 3
+INSTRUCTIONS: 
+        {
+        question: "What does CSS stand for?",
+        answers: ["Cascading Style Sheets", "Creative Style System", "Computer Style Syntax", "Coloured Screen Sheets"],
+        correct: 0
+        }
+- It receives a single prop called question (the object shape described above). 
+- It renders the question text and four answer buttons.
+- When a button is clicked, it highlights the correct answer green and the wrong answer red for 1 second, then emits "answer" with true (correct) or false (wrong).
+    - Is there a v-for on the buttons with a :key?
+    - Is there a data() property managing which button is selected and which is correct?
+    - Is setTimeout used with a 1-second delay?
+    - Does it $emit('answer', true/false)?
+-->
+
+<template> 
+    <div class = "question-card"> <!--question-card div that renders the question text-->
+        <p>{{question.question}}</p>
+        <!--for each answer in the array ANSWERS, make one button automatically, instead of writing 4 separate BUTTON tags-->
+        <!--:KEY creates a unique identifier for each button it creates -->
+        <!--when button 0 is clicked it calls selectAnswer(0) -->
+        <!--:CLASS - this button was clicked AND it is the right answer ... this button was clocked AND it is the wrong answer
+            inde = buttons number, selectedIndex = the button clicked, question.correct = the number of the right nswer (0
+            correct: is the class name from my CSS that I want to turn on or off from these conditions)
+            -->
+        <button v-for="(answer, index) in question.answers":key="index" 
+                v-on:click="selectAnswer(index)"
+                :class="{correct: index === selectedIndex && index === question.correct, wrong: index === selectedIndex && index !== question.correct}">
+                {{ answer }}
+        </button> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuestionCard',
+
+    data() {
+        return {
+            question: {
+                question: "What does CSS stand for?",
+                answers: [ "Cascading Style Sheets", "Creative Style System", "Computer Style Syntax", "Coloured Screen Sheets"],
+                correct: 0
+            },
+            selectedIndex: null //means this variable starts empty because its job is to remember which button the user clicked
+        }
+    },
+
+    // props: {
+    //     question: String
+    // },
+
+    methods: {
+        //This function runs when a button is clicked. INDEX here is whatever number got passes in from the click.
+        selectAnswer(index){
+            this.selectedIndex = index //this updates the empty variable from before
+            
+            //this takes two things, a function to run after the delay and the delay in milliseonds
+            //setTimeout(() => { stuff that happens after 1 second }, 1000)
+            setTimeout(() => {
+                //this following line does the actual checking, calculates true or false and stores it in isCorrect
+                const isCorrect = index == this.question.correct //QUESTION.CORRECT is hardcoded to 0 so if it is equal to zero
+                this.$emit('answer', isCorrect) //this then takes the true or false that was already calculated and sends it to the parent
+                this.selectedIndex = null //resets the selectedIndex
+            }, 1000)
+        }
+    }
+}
+</script>
